ffcb52d27fd7def8f63d52909ab1dc4b6b9ce033
Emerson Clementino <emdantascl@gmail.com>
2018-10-29 13:02:42 -0300

    update

---
+++ un02_prt03/src/un02_prt03/FenwickTree.java
@@ -0,0 +1,53 @@
+package un02_prt03;
+
+class FenwickTree {
+	int value, leftSize;
+	FenwickTree  left, right;
+
+	
+	public FenwickTree(int _value){
+		this.value=_value;
+		this.left=null;
+		this.right=null;
+	}
+	
+	public FenwickTree(int _leftSize, FenwickTree _left, FenwickTree _right){
+		this.leftSize=_leftSize;
+		this.left=_left;
+		this.right=_right;
+		this.value+=_left.value+_right.value;
+	}
+	
+	public String toString() {
+		String buffer="";
+		
+		buffer+="["+String.valueOf(this.value);
+		buffer+=", "+String.valueOf(this.leftSize);
+		
+		if(this.left!=null) {
+			buffer+=", "+this.left.toString();
+		}
+		
+		if(this.right!=null) {
+			buffer+=", "+this.right.toString();
+		}
+		
+		buffer+="]";
+		
+		return buffer;
+	}
+	
+	static FenwickTree allZeros(int n){
+		if (n==0) { return null;}
+		if (n==1) {return new FenwickTree(0);}
+		int m = n/2;
+		return new FenwickTree(n-m, allZeros(n-m), allZeros(m));
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Construcao de allZeros(3) : " + FenwickTree.allZeros(3));
+		System.out.println("Construcao de allZeros(4) : " + FenwickTree.allZeros(4));
+		System.out.println("Construcao de allZeros(5) : " + FenwickTree.allZeros(5));
+		System.out.println("Construcao de allZeros(6) : " + FenwickTree.allZeros(6));
+	}
+}
---
+++ un02_prt03/src/un02_prt03/Test.java
@@ -0,0 +1,12 @@
+package un02_prt03;
+
+public class Test{
+	public static void main(String[] args){
+		System.out.println("Construcao de FenwickTree(3) : " + new FenwickTree(3));
+		System.out.println("Construcao da arvore da figura : " +
+				new FenwickTree(3, new FenwickTree(1, new FenwickTree(4),
+						new FenwickTree(1, new FenwickTree(2), new FenwickTree(5))),
+						new FenwickTree(1, new FenwickTree(3),
+						new FenwickTree(1, new FenwickTree(6), new FenwickTree(1)))));
+	}
+}
